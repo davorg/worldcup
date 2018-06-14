98ef5f4916fdc26fa49efeecf543776ba4654a69
Dave Cross <dave@dave.org.uk>
2018-06-14 14:38:22 +0100

    Display current and past games differently.

--- docs/js/worldcup.js
+++ docs/js/worldcup.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
   var yourtimezone = moment.tz.guess();
   $('#yourtimezone').html(yourtimezone);
+  var now = moment();
 
   for (i = 0; i < games.length; i++) {
     var stadium = stadiums[games[i].stadium];
+    var time = moment.tz(games[i].date, 'UTC');
+    games[i].class = games[i].class + ' ' + game_class(now, games[i]);
     games[i].stadium = stadium.name;
     games[i].city = stadium.city;
-    var time = moment.tz(games[i].date, 'UTC');
     games[i].timeutc = time.format('llll');
     games[i].timestadium = time.clone().tz(stadium.timezone).format('llll');
     games[i].timeuser = time.clone().tz(yourtimezone).format('llll');
@@ -21,3 +23,16 @@ $(document).ready(function() {
     $('#list').html(rendered);
   });
 });
+
+function game_class(now, a_game) {
+  var game_start = moment.tz(a_game.date, 'UTC');
+  var game_end   = game_start.clone().add(105, 'minutes');
+
+  if (game_end < now) {
+    return 'past';
+  }
+  if (game_start <= now && now <= game_end) {
+    return 'present';
+  }
+  return 'future';
+}
